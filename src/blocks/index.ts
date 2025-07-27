468f50b8258a4411af3f376511ada1e88420ad17
Aziz Sobirov <azizsobirovhazarasp@gmail.com>
2025-07-27 22:16:29 +0500

    Add Partners section with SVG logos and styling
    - Created a new Partners.astro component to display partner logos.
    - Added SVG logo for partner 3.
    - Implemented responsive styles for the partners section, including layout adjustments for different screen sizes.
    - Enhanced visual presentation with a gradient background and improved text styling.

--- src/blocks/index.ts
+++ src/blocks/index.ts
@@ -1,6 +1,7 @@
 export { default as Clinic } from "./Clinic.astro";
 export { default as Founder } from "./Founder.astro";
 export { default as Hero } from "./Hero.astro";
+export { default as Partners } from "./Partners.astro";
 export { default as Result } from "./Result.astro";
 export { default as Services } from "./Services.astro";
 export { default as Specialists } from "./Specialists.astro";
